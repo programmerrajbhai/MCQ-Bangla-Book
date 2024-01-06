894a29d4bb4363f3dfc014e331b05b0c2a6d43e6
Progrsmmer_Raj <mdrajahmad686@gmail.com>
2024-01-05 21:52:17 -0800

    Final result Bangla Book

--- app/src/main/java/com/soft/mcqbanglabook/BanglaBook.java
+++ app/src/main/java/com/soft/mcqbanglabook/BanglaBook.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,25 +16,72 @@ public class BanglaBook extends AppCompatActivity {
         setContentView(R.layout.bangla_book);
 
 
-        Button shuva= findViewById(R.id.shuva);
+        TextView shuva= findViewById(R.id.shuva);
+        TextView boipora= findViewById(R.id.boipora);
+        TextView fulerbibaho= findViewById(R.id.fulerbibaho);
+        TextView protupokar= findViewById(R.id.protupokar);
+        TextView libery= findViewById(R.id.libery);
+        TextView denapawna= findViewById(R.id.denapawna);
+        TextView pollisahitto= findViewById(R.id.pollisahitto);
+        TextView AmAirVepuQNA= findViewById(R.id.AmAirVepuQNA);
+        TextView ManusMuhammod= findViewById(R.id.ManusMuhammod);
+        TextView NimGas= findViewById(R.id.NimGas);
+        TextView Shikkamonosotto= findViewById(R.id.Shikkamonosotto);
+        TextView momotadi= findViewById(R.id.momotadi);
+        TextView AkattorErdinGuli= findViewById(R.id.AkattorErdinGuli);
+        TextView SahittorRup= findViewById(R.id.SahittorRup);
+        TextView ProbasBondhu= findViewById(R.id.ProbasBondhu);
+        TextView jibonsongit= findViewById(R.id.jibonsongit);
+        TextView seidinaimat= findViewById(R.id.seidinaimat);
+        TextView amrporichoy= findViewById(R.id.amrporichoy);
+        TextView TomakePawerJonno= findViewById(R.id.TomakePawerJonno);
+        TextView kopotakkho= findViewById(R.id.kopotakkho);
+        TextView polliJononi= findViewById(R.id.polliJononi);
 
-        Button boipora= findViewById(R.id.boipora);
-        Button fulerbibaho= findViewById(R.id.fulerbibaho);
-        Button protupokar= findViewById(R.id.protupokar);
-        Button libery= findViewById(R.id.libery);
-        Button denapawna= findViewById(R.id.denapawna);
-        Button pollisahitto= findViewById(R.id.pollisahitto);
-        Button AmAirVepuQNA= findViewById(R.id.AmAirVepuQNA);
-        Button ManusMuhammod= findViewById(R.id.ManusMuhammod);
-        Button NimGas= findViewById(R.id.NimGas);
-        Button Shikkamonosotto= findViewById(R.id.Shikkamonosotto);
-        Button momotadi= findViewById(R.id.momotadi);
-        Button AkattorErdinGuli= findViewById(R.id.AkattorErdinGuli);
-        Button SahittorRup= findViewById(R.id.SahittorRup);
-        Button ProbasBondhu= findViewById(R.id.ProbasBondhu);
 
 
 
+        polliJononi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BanglaBook.this, PolliJononi.class));
+            }
+        });
+
+
+        kopotakkho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BanglaBook.this, KopotakkhoNod.class));
+            }
+        });
+
+        TomakePawerJonno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BanglaBook.this, TmakePawerJonno.class));
+            }
+        });
+
+        amrporichoy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BanglaBook.this, AmrPoricoy.class));
+            }
+        });
+
+        seidinaimat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BanglaBook.this, Sei_DinAimat.class));
+            }
+        });
+        jibonsongit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BanglaBook.this,jibonSongit.class));
+            }
+        });
         SahittorRup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
---
+++ app/src/main/java/com/soft/mcqbanglabook/PolliJononi.java
@@ -0,0 +1,142 @@
+package com.soft.mcqbanglabook;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+public class PolliJononi extends AppCompatActivity implements View.OnClickListener {
+
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button ansA, ansB, ansC, ansD;
+    Button submitBtn;
+    //==============
+
+    int score= 0;
+    int totalQuestion = PolliJononiQNA.myQuestion.length;
+    int currentQuestionIndex= 0;
+
+    String selectedAnswer= "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_polli_jononi);
+
+
+
+        totalQuestionsTextView = findViewById(R.id.total_question);
+        questionTextView = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_A);
+        ansB = findViewById(R.id.ans_B);
+        ansC = findViewById(R.id.ans_C);
+        ansD = findViewById(R.id.ans_D);
+        submitBtn = findViewById(R.id.submit_btn);
+
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        submitBtn.setOnClickListener(this);
+
+
+
+        totalQuestionsTextView.setText("Total Question:"+totalQuestion);
+
+        LoadNewQuestion();
+
+
+
+
+
+    }
+
+
+
+    private void LoadNewQuestion (){
+
+
+
+        if(currentQuestionIndex == totalQuestion ){
+            finishQuiz();
+            return;
+        }
+
+
+    /*    questionTextView.setText(BoiPora_QNA.myQuestion[currentQuestionIndex]);
+        ansA.setText(BoiPora_QNA.myChoices[currentQuestionIndex][0]);
+        ansB.setText(BoiPora_QNA.myChoices[currentQuestionIndex][1]);
+        ansC.setText(BoiPora_QNA.myChoices[currentQuestionIndex][2]);
+        ansD.setText(BoiPora_QNA.myChoices[currentQuestionIndex][3]);
+
+*/
+
+        questionTextView.setText(PolliJononiQNA.myQuestion[currentQuestionIndex]);
+        ansA.setText(PolliJononiQNA.myChoices[currentQuestionIndex][0]);
+        ansB.setText(PolliJononiQNA.myChoices[currentQuestionIndex][1]);
+        ansC.setText(PolliJononiQNA.myChoices[currentQuestionIndex][2]);
+        ansD.setText(PolliJononiQNA.myChoices[currentQuestionIndex][3]);
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        ansA.setBackgroundColor(Color.WHITE);
+        ansB.setBackgroundColor(Color.WHITE);
+        ansC.setBackgroundColor(Color.WHITE);
+        ansD.setBackgroundColor(Color.WHITE);
+
+        Button clickedButton = (Button) view;
+        if(clickedButton.getId()==R.id.submit_btn){
+            if(selectedAnswer.equals(PolliJononiQNA.myCorrectAnswer[currentQuestionIndex])){
+                score++;
+            }
+            currentQuestionIndex++;
+            LoadNewQuestion();
+
+
+        }else{
+            //choices button clicked
+            selectedAnswer  = clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(Color.MAGENTA);
+
+        }
+
+    }
+
+
+    void finishQuiz(){
+        String passStatus = "";
+        if(score > totalQuestion*0.60){
+            passStatus = "Passed";
+        }else{
+            passStatus = "Failed";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Score is "+ score+" out of "+ totalQuestion)
+                .setPositiveButton("Restart",(dialogInterface, i) -> restartQuiz() )
+                .setCancelable(false)
+                .show();
+
+
+    }
+
+    void restartQuiz(){
+        score = 0;
+        currentQuestionIndex =0;
+        LoadNewQuestion();
+    }
+
+}
